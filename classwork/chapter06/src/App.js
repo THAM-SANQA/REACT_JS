f660f13e8d01f13ab89fc8a3d5fb366170649b8f
THAM-SANQA <ndabat020@gmail.com>
2023-09-29 16:55:20 +0200

    first commit

--- classwork/chapter06/src/App.js
+++ classwork/chapter06/src/App.js
@@ -2,13 +2,10 @@
 //chapter6
 import React, { Component } from "react";
 import UserForm from "./UserForm";
-import GitHub from './GitHub';
-
 class App extends Component {
   render() {
     return (
       <div>
-        <GitHub />
         <UserForm />
       </div>
     );
