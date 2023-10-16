cf1dbbc277362277e4235ee14e2f01c866887cf7
THAM-SANQA <ndabat020@gmail.com>
2023-10-16 11:59:18 +0200

    first commit

---
+++ projects/shopping-cart-app/src/data.js
@@ -0,0 +1,28 @@
+const data ={
+    products: [
+        {
+            id: "1",
+            name: "Asus Laptop",
+            price: 2400,
+            image: "https://picsum.photos/id/180/2400/1600",
+        },
+
+        {
+            id: "2",
+            name: "Vintage Car",
+            price: 3500,
+            image: "https://picsum.photos/id/111/4400/2656",
+        },
+
+        {
+            id: "3",
+            name: "W Shoes",
+            price: 1500,
+            image: "https://picsum.photos/id/21/3008/2008",
+        },
+
+       
+    ]
+
+}
+export default data;
